b8d5f41a43bfd24230da0809b1480fe3cac66afd
Maulana Adil <maulanaadil.latif@gmail.com>
2022-03-06 17:31:43 +0700

    change shopPage collection into container pattern and convert collection page style into styled components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import './collection.style.scss';
+const CollectionPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h2`
+  font-size: 38px;
+  margin: 0 auto 30px;
+`;
+
+const Items = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`;
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
+    <CollectionPageContainer>
+      <Title>{title}</Title>
+      <Items>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </Items>
+    </CollectionPageContainer>
   );
 };
 
